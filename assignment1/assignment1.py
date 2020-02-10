5b02fc86ce310a958f15de969755762b84b3816c
337167503@qq.com <jia@GUOdeMacBook-Pro.local>
2020-02-10 21:07:11 +0800

    make it better

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -2,13 +2,12 @@ from __future__ import unicode_literals
 
 import redis
 
-
 # fill in the following.
-HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
-PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
-PORT = "11363" 
+HOST = "redis-16933.c15.us-east-1-4.ec2.cloud.redislabs.com"
+PWD = "yBFMT0bNR2mpF3HjviE45Ig9xGIPxKtI"
+PORT = "16933"
 
-redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
+redis1 = redis.Redis(host=HOST, password=PWD, port=PORT)
 
 while True:
     msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
@@ -16,11 +15,16 @@ while True:
         break
     if msg == '':
         continue
-    print("You have entered " + msg, end=' ') 
+    print("You have entered " + msg, end=' ')
 
-   
     # Add your code here
 
+    try:
+        X=redis1.get(msg)
+        count=int(X.decode())
+        redis1.incr(msg)
+        print('for',count,' times')
+    except:
+        redis1.incr(msg)
+        print('for zero times')
 
-
-    print('for X times')
